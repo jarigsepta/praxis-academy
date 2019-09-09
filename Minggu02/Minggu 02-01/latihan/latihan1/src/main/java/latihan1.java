5d19ee16dda00ff1a06397bf77b96633a9296041
jarigsepta <jarigsepta8@gmail.com>
2019-09-09 15:27:23 +0700

    penambahan latihan

---
+++ Minggu02/Minggu 02-01/latihan/latihan1/src/main/java/latihan1.java
@@ -0,0 +1,13 @@
+public class latihan1 {
+    static int jumlah (int a, int b){
+           return a + b;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(
+        "Hasil Penjumlahan 10+10 adalah "+jumlah(10, 10)
+        );
+    }
+}
---
+++ Minggu02/Minggu 02-01/latihan/latihan1/src/main/java/latihan1/MyInterface2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author jarig
+ */
+public interface MyInterface2 {
+    public void run();
+
+    public default void doIt() {
+        System.out.println("doing it");
+    }
+
+    public static void doItStatically() {
+        System.out.println("doing it statically");
+    }
+}

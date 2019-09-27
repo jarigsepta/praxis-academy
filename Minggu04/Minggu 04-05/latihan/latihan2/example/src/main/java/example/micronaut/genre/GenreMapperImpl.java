5bb67dfacabed264679701f4f640f162d18e901e
jarigsepta <jarigsepta8@gmail.com>
2019-09-27 17:06:18 +0700

    penambahan minggu 04-05

---
+++ Minggu04/Minggu 04-05/latihan/latihan2/example/src/main/java/example/micronaut/genre/GenreMapperImpl.java
@@ -0,0 +1,88 @@
+package example.micronaut.genre;
+
+import example.micronaut.domain.Genre;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import javax.inject.Singleton;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@Singleton
+public class GenreMapperImpl implements GenreMapper {
+
+    private final SqlSessionFactory sqlSessionFactory;
+
+    public GenreMapperImpl(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    @Override
+    public Genre findById(long id) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return getGenreMapper(sqlSession).findById(id);
+        }
+    }
+
+    private GenreMapper getGenreMapper(SqlSession sqlSession) {
+        return sqlSession.getMapper(GenreMapper.class);
+    }
+
+    @Override
+    public void save(Genre genre) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            getGenreMapper(sqlSession).save(genre);
+            sqlSession.commit();
+        }
+    }
+
+    @Override
+    public void deleteById(long id) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            getGenreMapper(sqlSession).deleteById(id);
+            sqlSession.commit();
+        }
+    }
+
+    @Override
+    public void update(long id, String name) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            getGenreMapper(sqlSession).update(id, name);
+            sqlSession.commit();
+        }
+    }
+
+    @Override
+    public List<Genre> findAll() {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return getGenreMapper(sqlSession).findAll();
+        }
+    }
+
+    @Override
+    public List<Genre> findAllBySortAndOrder(@NotNull @Pattern(regexp = "id|name") String sort,
+            @NotNull @Pattern(regexp = "asc|ASC|desc|DESC") String order) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return getGenreMapper(sqlSession).findAllBySortAndOrder(sort, order);
+        }
+    }
+
+    @Override
+    public List<Genre> findAllByOffsetAndMaxAndSortAndOrder(@NotNull @PositiveOrZero Integer offset,
+            @Positive @NotNull Integer max, @NotNull @Pattern(regexp = "id|name") String sort,
+            @NotNull @Pattern(regexp = "asc|ASC|desc|DESC") String order) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return getGenreMapper(sqlSession).findAllByOffsetAndMaxAndSortAndOrder(offset, max, sort, order);
+        }
+    }
+
+    @Override
+    public List<Genre> findAllByOffsetAndMax(@NotNull @PositiveOrZero Integer offset, @Positive @NotNull Integer max) {
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            return getGenreMapper(sqlSession).findAllByOffsetAndMax(offset, max);
+        }
+    }
+}
